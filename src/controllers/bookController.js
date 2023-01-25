fc7a50de91dd6109ac093fd606fb5d728c01328b
vpalve07 <vinayak.palve02@gmail.com>
2023-01-25 12:54:35 +0530

    3rdDay-Commit3

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -24,7 +24,10 @@ const book = async function (req, res) {
         if (!validUser) return res.status(400).send({ status: false, msg: "User not found with the provided UserID" })
 
         const isbnRegex = /^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/i
-        if (!isbnRegex.test(data.ISBN)) return res.status(400).send({ status: false, msg: "ISBN number format is incorrecr 'ISBN number can be of either 10 or 13 digits and ISBN-13 starts with 978 or 979' some examples are - 1. (ISBN-10: 0-306-40615-2) 2. (ISBN-13: 978-0-306-40615-7) 3. (ISBN-13 with spaces: 978 0 306 40615 7)  4. (ISBN-13 with hyphens: 978-0-306-40615-7)  5. (ISBN-13 with ISBN prefix: ISBN 978-0-306-40615-7)  6. (ISBN-13 with ISBN-13 prefix: ISBN-13 978-0-306-40615-7)" })
+        if (!isbnRegex.test(data.ISBN)) return res.status(400).send({ status: false, msg: "ISBN number format is incorrect 'ISBN number can be of either 10 or 13 digits and ISBN-13 starts with 978 or 979' some examples are - 1. (ISBN-10: 0-306-40615-2) 2. (ISBN-13: 978-0-306-40615-7) 3. (ISBN-13 with spaces: 978 0 306 40615 7)  4. (ISBN-13 with hyphens: 978-0-306-40615-7)  5. (ISBN-13 with ISBN prefix: ISBN 978-0-306-40615-7)  6. (ISBN-13 with ISBN-13 prefix: ISBN-13 978-0-306-40615-7)" })
+
+        let findISBN = await bookModel.findOne({ISBN:ISBN})
+        if (!findISBN) return res.status(400).send({ status: false, msg: "ISBN number already exists" })
 
         let createBook = await bookModel.create(data)
         return res.status(201).send({ status: true, message: 'Success', data: createBook })
