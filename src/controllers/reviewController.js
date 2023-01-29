be6a8a0117d4d05efff614fe8b0c3adb6847b554
vpalve07 <vinayak.palve02@gmail.com>
2023-01-29 13:29:00 +0530

    almostFinal-commit

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -12,7 +12,7 @@ const review = async function (req, res) {
         if (!data.rating) return res.status(400).send({ status: false, msg: "rating is mandatory" })
         if (data.rating >= 5.1 || data.rating < 1) return res.status(400).send({ status: false, msg: "please rate in between 1 to 5" })
         data.rating = Math.round(data.rating)
-
+        if(data.review) if(data.review == "") return res.status(400).send({ status: false, msg: "review field can not be empty" })
 
         if (!mongoose.isValidObjectId(data.bookId)) return res.status(400).send({ status: false, msg: "bookId is invalid" })
 
@@ -35,7 +35,7 @@ const updateReview = async function (req, res) {
         if ((!mongoose.isValidObjectId(req.params.bookId)) || (!mongoose.isValidObjectId(req.params.reviewId))) return res.status(400).send({ status: false, msg: "bookId or reviewId is invalid" })
         if (data.rating > 5 || data.rating < 1) return res.status(400).send({ status: false, msg: "please rate in between 1 to 5" })
         data.rating = Math.round(data.rating)
-        
+
         let findBook = await bookModel.findOne({ _id: req.params.bookId, isDeleted: false })
         if (!findBook) return res.status(404).send({ status: false, msg: "No book found" })
         let { _id, title, excerpt, userId, category, subcategory, isDeleted, reviews, releasedAt, createdAt, updatedAt } = findBook
